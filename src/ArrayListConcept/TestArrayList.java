1f143bdbebbdceb8d5dd6eafd49dc24fa312919e
leenarani <samalrani92@gmail.com>
2022-08-10 20:46:01 +0530

    changes are updated

--- src/ArrayListConcept/TestArrayList.java
+++ src/ArrayListConcept/TestArrayList.java
@@ -5,6 +5,8 @@ public class TestArrayList {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ABC");
+		System.out.println("By locator");
+		System.out.println("ABC");
 
 	}
 
