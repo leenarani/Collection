4036c78c33bbc66ab8ce05de9fec5a3c4df8e546
leenarani <samalrani92@gmail.com>
2022-08-10 20:18:51 +0530

    TestArrayList added

---
+++ src/ArrayListConcept/TestArrayList.java
@@ -0,0 +1,11 @@
+package ArrayListConcept;
+
+public class TestArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ABC");
+
+	}
+
+}
